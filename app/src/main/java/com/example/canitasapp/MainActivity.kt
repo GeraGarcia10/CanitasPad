9560ba20b3632a0c1f94fcfbcb93ad9268dd228d
GeraGarcia10 <s22120009@alumnos.itsur.edu.mx>
2024-10-06 12:43:35 -0600

    Se implementa la interfaz principal con pestañas para tareas y notas

--- app/src/main/java/com/example/canitasapp/MainActivity.kt
+++ app/src/main/java/com/example/canitasapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.canitasapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -28,19 +34,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(modifier: Modifier = Modifier) {
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val tabs = listOf("Tasks", "Notes")
+    var taskList by remember { mutableStateOf(mutableListOf("Task 1", "Task 2")) }
+    var noteList by remember { mutableStateOf(mutableListOf("Note 1", "Note 2")) }
+    var searchQuery by remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CanitasAppTheme {
-        Greeting("Android")
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        CanitasAppTheme {
+            Greeting("Android")
+        }
     }
 }
